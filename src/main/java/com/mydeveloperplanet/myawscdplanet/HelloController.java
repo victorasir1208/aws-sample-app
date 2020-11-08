d4b82e4ff82fb5bd3878630a921747c50013d784
mydeveloperplanet <gunter@mydeveloperplanet.com>
2020-11-08 16:02:53 +0100

    Changed hello message text

--- src/main/java/com/mydeveloperplanet/myawscdplanet/HelloController.java
+++ src/main/java/com/mydeveloperplanet/myawscdplanet/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        String message = "Hello AWS Continuous Delivery!";
+        String message = "Hello again AWS Continuous Delivery!";
         try {
             InetAddress ip = InetAddress.getLocalHost();
             message += " From host: " + ip;
