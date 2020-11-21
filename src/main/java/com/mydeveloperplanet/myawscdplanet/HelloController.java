64c078fa99797e09353065b898a3ad0206f4a6ea
mydeveloperplanet <gunter@mydeveloperplanet.com>
2020-11-21 11:07:07 +0100

    Changed the text for testing the Review stage

--- src/main/java/com/mydeveloperplanet/myawscdplanet/HelloController.java
+++ src/main/java/com/mydeveloperplanet/myawscdplanet/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        String message = "Hello again AWS Continuous Delivery!";
+        String message = "Hello AWS Continuous Delivery with Review Stage!";
         try {
             InetAddress ip = InetAddress.getLocalHost();
             message += " From host: " + ip;
